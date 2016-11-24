97bbcad9609554647a0b2daf4bef9cbb0ac12daa
andre <andre@DESKTOP-NIHUKT8.lan>
2016-11-24 16:38:07 +0100

    Completato esercitazione

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,8 +2,86 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
-	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
+	private static final int lenghtSolution = 81;
+
+	public int verify(String candidateSolution) throws IllegalSizeSolution {
+		char[] solutionArray = candidateSolution.toCharArray();
+		int sizeSolution = solutionArray.length;
+		if (sizeSolution != lenghtSolution)
+			throw new IllegalSizeSolution();
+
+		if (!validCharInput(solutionArray))
+			return -1;
+		if (!validSubGrid(solutionArray))
+			return -2;
+		if (!validSubRow(candidateSolution))
+			return -3;
+		if (!validSubColumn(candidateSolution))
+			return -4;
+
 		return 0;
 	}
+
+	private boolean validCharInput(char[] solutionArray) {
+		for (char c : solutionArray) {
+			if (c != '1' && c != '2' && c != '3' && c != '4' && c != '5' && c != '6' && c != '7' && c != '8'
+					&& c != '9')
+				return false;
+		}
+		return true;
+	}
+
+	private boolean validSubGrid(char[] solutionArray) {
+		String subGrid;
+		int i = 0;
+		while (i < 61) {
+			subGrid = "" + solutionArray[i] + solutionArray[i + 1] + solutionArray[i + 2] + solutionArray[i + 9]
+					+ solutionArray[i + 10] + solutionArray[i + 11] + solutionArray[i + 18] + solutionArray[i + 19]
+					+ solutionArray[i + 20];
+			if (!diversityOfCharOfString(subGrid))
+				return false;
+			if (i == 6)
+				i = 27;
+			else if (i == 33)
+				i = 54;
+			else
+				i += 3;
+		}
+		return true;
+	}
+
+	public boolean validSubRow(String candidateSolution) {
+		String temp;
+		for (int i = 0; i < lenghtSolution; i += 9) {
+			temp = candidateSolution.substring(i, i + 9);
+			if (!diversityOfCharOfString(temp))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean validSubColumn(String candidateSolution) {
+		char[] solutionArray=candidateSolution.toCharArray();
+		String temp;
+		for (int i = 0; i < 9; i++) {
+			temp ="" + solutionArray[i] + solutionArray[i+9] + solutionArray[i+18] + solutionArray[i+27]
+							+ solutionArray[i+36] + solutionArray[i+45] + solutionArray[i+54] + solutionArray[i+63]
+							+ solutionArray[i+72];
+			if (!diversityOfCharOfString(temp))
+				return false;
+		}
+		return true;
+	}
+
+	private boolean diversityOfCharOfString(String subGrid) {
+		char[] subGridArray = subGrid.toCharArray();
+		for (int y = 0; y < 8; y++) {
+			for (int z = y + 1; z < 9; z++) {
+				if (subGridArray[y] == subGridArray[z])
+					return false;
+			}
+		}
+		return true;
+	}
+
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,80 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	SudokuVerifier sudoku;
+
+	@Before
+	public void setUp() throws Exception {
+		sudoku = new SudokuVerifier();
 	}
 
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test(expected = IllegalSizeSolution.class)
+	public void addSolutionIllegalSizeSolutionException() throws IllegalSizeSolution {
+		// Arrange
+		// Act
+		sudoku.verify("23648957694");
+	}
+
+	@Test
+	public void validSolutionString() throws IllegalSizeSolution {
+		// Arrange
+		int result;
+		// Act
+		result = sudoku.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		// Assert
+		assertEquals(0, result);
+	}
+
+
+	@Test
+	public void invalidCharacterOfString() throws IllegalSizeSolution {
+		// Arrange
+		int result;
+		// Act
+		result = sudoku.verify("407369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		// Assert
+		assertEquals(-1, result);
+	}
+
+	@Test
+	public void invalidSubGridOfString() throws IllegalSizeSolution {
+		// Arrange
+		int result;
+		// Act
+		result = sudoku.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875299");
+		// Assert
+		assertEquals(-2, result);
+	}
+
+	@Test
+	public void invalidRowOfString() throws IllegalSizeSolution {
+		// Arrange
+		boolean result;
+		// Act
+		result = sudoku.validSubRow("417369825632158947958724316825437169791586432346912758289643571573291684164875299");
+		// Assert
+		assertFalse(result);
+	}
+
+	@Test
+	public void invalidColumnOfString() throws IllegalSizeSolution {
+		// Arrange
+		boolean result;
+		// Act
+		result = sudoku.validSubColumn("417369825632158947958724316825437169791586432346912758289643571573291684164875299");
+		// Assert
+		assertFalse(result);
+	}
+	
+	
 }
