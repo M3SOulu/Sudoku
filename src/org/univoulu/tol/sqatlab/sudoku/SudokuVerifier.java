08061441c744e44f64389446d0f8e4018598db01
michelecafagna26 <mikele.c@outlook.it>
2016-11-24 17:26:29 +0100

    firstrule secondrule

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,9 +1,106 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
+import java.util.ArrayList;
+
 public class SudokuVerifier {
 
+	public static int MAX_SOLUTION_SIZE = 81;
+
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
-		return 0;
+
+		int result = 0;
+
+		if (!checkFirstRule(candidateSolution))
+			result = -1;
+		
+
+		return result;
 	}
+
+	/**
+	 * if first rule is respected return true, false otherwise
+	 * 
+	 * @param candidateSolution
+	 * @return
+	 */
+	private boolean checkFirstRule(String candidateSolution) {
+
+		boolean result = true;
+
+		if (candidateSolution.length() != MAX_SOLUTION_SIZE) {
+
+			result = false;
+
+		} else {
+
+			for (int i = 0; i < MAX_SOLUTION_SIZE; i++) {
+
+				if (!Character.isDigit(candidateSolution.charAt(i))) {
+
+					result = false;
+					break;
+				}
+
+			}
+
+		}
+
+		return result;
+	}
+
+	/**
+	 * if second rule is respected return true, false otherwise
+	 * 
+	 * @param candidateSolution
+	 * @return
+	 */
+	private boolean checkSecondRule(String candidateSolution) {
+
+		// TODO
+
+		boolean result = true;
+
+		return result;
+	}
+	
+	/**
+	 * extract sub grids
+	 * @param candidateSolution
+	 * @return arrayList that contains the 9 subgrids planed on a string
+	 */
+	private ArrayList<String> getSubGrid(String candidateSolution){
+		
+		int gridShift=3;
+		int init_start=0;
+		int init_end=init_start+3;
+		int shift=9;
+		
+		ArrayList<String> subGrids= new ArrayList<String>(9);
+		
+		int start=init_start;
+		int end=init_end;
+			
+		for(int k=0;k<9;k++){
+			
+			String tempGrid="";
+			
+			for(int i=0;i<3;i++){
+				
+				tempGrid+=candidateSolution.substring(start,end);
+				
+				start+=shift;
+				end+=shift;
+			}
+			
+			subGrids.add(tempGrid);
+			
+			start=gridShift;
+			end=start+3;
+			gridShift+=3;
+		}
+		
+			
+		return subGrids;
+	}
+
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,50 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	SudokuVerifier sudoVer;
+	
+	
+	@Before
+	public void setUp(){
+		
+		 sudoVer= new SudokuVerifier();
 	}
+	
+	//check first rule
+	@Test
+	public void TestSolutionLength() {		
+		
+		//size:
+		//0
+		assertEquals(-1,sudoVer.verify(""));
+		
+		//80
+		assertEquals(-1,sudoVer.verify("41736982563215894795872431682543716979158643234691275828964357157329168416487529"));
+		
+		//82
+		assertEquals(-1,sudoVer.verify("4173698256321589479587243168254371697915864323469127582896435715732916841648752930"));
+		
+		//81
+		assertEquals(0,sudoVer.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
+		
+		
+	}
+	
+	@Test
+	public void testNotDigitChars(){
+		
+		String candidateSolution="41736982563215894795872431682543716979158-432346912758289643571573291684164875293";
+		
+		assertEquals(-1,sudoVer.verify(candidateSolution));
+	}
+	
+	
 
 }
