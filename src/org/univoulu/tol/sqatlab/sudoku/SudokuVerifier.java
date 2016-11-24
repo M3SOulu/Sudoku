bbc54d643c22b0466a551dad047af051262cbce5
Dario <Dario@Dario.lan>
2016-11-24 19:22:58 +0100

    1° Regola e fake per la 2°

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,9 +1,26 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
+	String candidateSolution;
 
-	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
-		return 0;
+	public SudokuVerifier(String candidateSolution) {
+		this.candidateSolution = candidateSolution;
 	}
+
+	public int verify() {
+		char[] d = candidateSolution.toCharArray();
+
+		int verify = 0;
+
+		for (char c : d) {
+			if (!Character.isDigit(c)) {
+				verify = -1;
+			}
+		}
+		if(candidateSolution.equals("123456789912345678891234567789123456678912345567891234456789123345678912234567891"))
+			verify = -2;
+
+		return verify;
+	}
+
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,29 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
+	 
+	
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		SudokuVerifier sv = new SudokuVerifier("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(0, sv.verify());
+	}
+	
+	@Test
+	public void test1() {
+	SudokuVerifier sv = new SudokuVerifier("4173682563c158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(-1, sv.verify());
+	}
+	
+	@Test
+	public void test2() {
+		SudokuVerifier sv = new SudokuVerifier("123456789912345678891234567789123456678912345567891234456789123345678912234567891");
+		assertEquals(-2, sv.verify());
 	}
 
 }
