a56d9ba1f0317cdd5d694e3e4cbc003ff6bd07f3
Dario <Dario@Dario.lan>
2016-11-24 23:22:00 +0100

    Implementata seconda Regola e fatto refactoring

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,6 +2,7 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 	String candidateSolution;
+	int verify = 0;
 
 	public SudokuVerifier(String candidateSolution) {
 		this.candidateSolution = candidateSolution;
@@ -10,17 +11,47 @@ public class SudokuVerifier {
 	public int verify() {
 		char[] d = candidateSolution.toCharArray();
 
-		int verify = 0;
-
 		for (char c : d) {
 			if (!Character.isDigit(c)) {
 				verify = -1;
 			}
 		}
-		if(candidateSolution.equals("123456789912345678891234567789123456678912345567891234456789123345678912234567891"))
-			verify = -2;
+
+		if (verify == 0) {
+
+			secondRule();
+			
+			
+
+		}
 
 		return verify;
+	}
+
+	private void check(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i + 1; j < s.length(); j++) {
+				if (s.charAt(i) == s.charAt(j))
+					verify = -2;
+			}
+		}
+
+	}
+
+	private String subGrid(int start) {
+
+		return candidateSolution.substring(start, start + 3)
+				.concat(candidateSolution.substring(start + 9, start + 9 + 3))
+				.concat(candidateSolution.substring(start + 9 + 9, start + 9 + 9 + 3));
+	}
+
+	private void secondRule() {
+		for (int i = 0; i < 7; i += 3) {
+			for (int j = i; j <= i + (27 * 2); j += 27) {
+				check(subGrid(j));
+			}
+		}
+
 	}
 
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -26,5 +26,11 @@ public class TestSudokuVerifier {
 		SudokuVerifier sv = new SudokuVerifier("123456789912345678891234567789123456678912345567891234456789123345678912234567891");
 		assertEquals(-2, sv.verify());
 	}
+	
+	@Test
+	public void test3() {
+		SudokuVerifier sv = new SudokuVerifier("123456789912345678891234567789123456678912345567891234456789123345678912234567891");
+		assertEquals(-2, sv.verify());
+	}
 
 }
