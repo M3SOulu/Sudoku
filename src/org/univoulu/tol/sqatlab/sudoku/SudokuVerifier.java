24a75d22b92e8126457c1faf55b288a0a6cc854b
maurizionapoliMN <maurizionapoli87@gmail.com>
2016-11-28 22:14:53 +0100

    Rule 1 verification implemented and tested

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,13 +1,32 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
-public class SudokuVerifier {
+import exceptions.NotValidSolutionLength;
 
-	public int verify(String candidateSolution) {
+public class SudokuVerifier {
+	
+
+	public static int verify(String candidateSolution) throws NotValidSolutionLength{
+		
+		if(candidateSolution.length()!=81)
+			throw new NotValidSolutionLength();
+		
+		int response = 0;
+		
 		// Return 0: means it is a valid Sudoku solution
+		
 		// Return -1: means it is violating Rule #1
+		for(int i = 0; i<candidateSolution.length(); i++){
+			Character nextChar = candidateSolution.charAt(i);
+			if(isAValidDigit(nextChar))
+				response = -1;
+		}
 		// Return -2: means it is violating Rule #2
 		// Return -3: means it is violating Rule #3
 		// Return -4: means it is violating Rule #4
-		return 0;
+		return response;
+	}
+
+	private static boolean isAValidDigit(Character nextChar) {
+		return !Character.isDigit(nextChar) || nextChar == '0';
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -4,11 +4,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import exceptions.NotValidSolutionLength;
+
 public class TestSudokuVerifier {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test(expected = NotValidSolutionLength.class)
+	public void stringShorterThan81ShouldRiseAnException() throws NotValidSolutionLength{
+		//Arrange
+		String solution = "123456789";
+		//Act
+		SudokuVerifier.verify(solution);
 	}
 
+	@Test(expected = NotValidSolutionLength.class)
+	public void stringLongerThan81ShouldRiseAnException() throws NotValidSolutionLength{
+		//Arrange
+		String solution = "123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789";
+		//Act
+		SudokuVerifier.verify(solution);
+	}
+
+	@Test
+	public void stringContainingNonDigitCharacterShouldReturnMinus1() throws NotValidSolutionLength{
+		//Arrange
+		String solution = "12345678912345678912345678912356789123456789123456789123456789123456789123456789a";
+		//Act & Assert
+		assertTrue(SudokuVerifier.verify(solution) == -1);
+	}
+	
+	@Test
+	public void stringContainZeroShouldReturnMinus1() throws NotValidSolutionLength{
+		//Arrange
+		String solution = "123456789123456789123456789123567891234567891234567891234567891234567891234567890";
+		//Act & Assert
+		assertTrue(SudokuVerifier.verify(solution) == -1);
+	}
 }
