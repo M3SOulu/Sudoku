a9a92d4ec105483e0ed9b9b5ccda21fd77249bb8
Michele <Michele@Michele_PC.homenet.telecomitalia.it>
2016-11-29 22:26:10 +0100

    fixed import

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -1,8 +1,6 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
