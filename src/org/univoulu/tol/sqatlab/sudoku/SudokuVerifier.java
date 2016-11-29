64b365f2b5800bb1f571370dfe18b008f185a51b
maurizionapoliMN <maurizionapoli87@gmail.com>
2016-11-29 01:12:05 +0100

    Rules 3 and 4 implemented and tested

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,30 +3,58 @@ package org.univoulu.tol.sqatlab.sudoku;
 import exceptions.NotValidSolutionLength;
 
 public class SudokuVerifier {
-	
 
-	public static int verify(String candidateSolution) throws NotValidSolutionLength{
-		
-		if(candidateSolution.length()!=81)
+	public char[][] grid;
+
+	public SudokuVerifier() {
+		grid = new char[9][9];
+	}
+
+	public int verify(String candidateSolution) throws NotValidSolutionLength {
+
+		if (candidateSolution.length() != 81)
 			throw new NotValidSolutionLength();
-		
+
+		// Return -1: means it is violating Rule #1
+		for (int i = 0; i < candidateSolution.length(); i++)
+			if (!isAValidDigit(candidateSolution.charAt(i)))
+				return -1;
+
 		int response = 0;
+
+		convertIntoGrid(candidateSolution);
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				char charToControl = grid[i][j];
+				
+				// Return -3: means it is violating Rule #3
+				for(int nextCharInTheRow = j+1; nextCharInTheRow<9; nextCharInTheRow++)
+					if(charToControl == grid[i][nextCharInTheRow])
+						return -3;
+				// Return -4: means it is violating Rule #4
+				for(int nextCharInTheColumn = i+1; nextCharInTheColumn<9; nextCharInTheColumn++)
+					if(charToControl == grid[nextCharInTheColumn][j])
+						return -4;
+			}
+		}
+
+		// Return -2: means it is violating Rule #2
+
+
 		
 		// Return 0: means it is a valid Sudoku solution
-		
-		// Return -1: means it is violating Rule #1
-		for(int i = 0; i<candidateSolution.length(); i++){
-			Character nextChar = candidateSolution.charAt(i);
-			if(isAValidDigit(nextChar))
-				response = -1;
-		}
-		// Return -2: means it is violating Rule #2
-		// Return -3: means it is violating Rule #3
-		// Return -4: means it is violating Rule #4
 		return response;
 	}
 
+	private void convertIntoGrid(String candidateSolution) {
+			for (int row = 0, i = 0; row < 9 && i < candidateSolution.length(); row++,  i += 9){
+				String newRow = candidateSolution.substring(i, i + 9);
+				grid[row] = newRow.toCharArray();
+			}
+	}
+
 	private static boolean isAValidDigit(Character nextChar) {
-		return !Character.isDigit(nextChar) || nextChar == '0';
+		return Character.isDigit(nextChar) && nextChar != '0';
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,41 +2,66 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import exceptions.NotValidSolutionLength;
 
 public class TestSudokuVerifier {
 
-	@Test(expected = NotValidSolutionLength.class)
-	public void stringShorterThan81ShouldRiseAnException() throws NotValidSolutionLength{
-		//Arrange
-		String solution = "123456789";
-		//Act
-		SudokuVerifier.verify(solution);
+	SudokuVerifier verifier;
+
+	@Before
+	public void setUp() throws Exception {
+		// Arrange
+		verifier = new SudokuVerifier();
 	}
 
 	@Test(expected = NotValidSolutionLength.class)
-	public void stringLongerThan81ShouldRiseAnException() throws NotValidSolutionLength{
-		//Arrange
-		String solution = "123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789";
-		//Act
-		SudokuVerifier.verify(solution);
+	public void stringShorterThan81ShouldRiseAnException() throws NotValidSolutionLength {
+		// Arrange
+		String solution = "123456789";
+		// Act
+		verifier.verify(solution);
+	}
+
+	@Test(expected = NotValidSolutionLength.class)
+	public void stringLongerThan81ShouldRiseAnException() throws NotValidSolutionLength {
+		// Arrange
+		String solution = "4173698256321589479587243168254371697915864323469127582896435715732916841648752938";
+		// Act
+		verifier.verify(solution);
 	}
 
 	@Test
-	public void stringContainingNonDigitCharacterShouldReturnMinus1() throws NotValidSolutionLength{
+	public void stringContainingNonDigitCharacterShouldReturnMinus1() throws NotValidSolutionLength {
+		// Arrange
+		String solution = "41736982563215894795872431682543716979158643234691275828964357157329168416487529a";
+		// Act & Assert
+		assertTrue(verifier.verify(solution) == -1);
+	}
+
+	@Test
+	public void stringContainZeroShouldReturnMinus1() throws NotValidSolutionLength {
+		// Arrange
+		String solution = "417369825632158947958724316825437169791586432346912758289643571573291684164875290";
+		// Act & Assert
+		assertTrue(verifier.verify(solution) == -1);
+	}
+
+	@Test
+	public void repetitionInARowShouldReturnMinus3() throws NotValidSolutionLength {
 		//Arrange
-		String solution = "12345678912345678912345678912356789123456789123456789123456789123456789123456789a";
+		String solution = "447369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		//Act & Assert
-		assertTrue(SudokuVerifier.verify(solution) == -1);
+		assertTrue(verifier.verify(solution) == -3);
 	}
 	
 	@Test
-	public void stringContainZeroShouldReturnMinus1() throws NotValidSolutionLength{
+	public void repetitionInAColumnShouldReturnMinus4() throws NotValidSolutionLength {
 		//Arrange
-		String solution = "123456789123456789123456789123567891234567891234567891234567891234567891234567890";
+		String solution = "417369825632158947958724316825437169791586432346912758289643571574291683164875293";
 		//Act & Assert
-		assertTrue(SudokuVerifier.verify(solution) == -1);
+		assertTrue(verifier.verify(solution) == -4);
 	}
 }
